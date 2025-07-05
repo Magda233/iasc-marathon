3cb665db020728e192b5e753cfc455fe79bce183
admin2 <https://github.com/Magda233/iasc-marathon>
2025-07-05 23:02:21 +0300

    lists_exercises.py

---
+++ lists_exercises.py
@@ -0,0 +1,29 @@
+# food = ["cheese", "eggs", "flour","milk","bread"]
+# food.append("butter")
+# print(food)
+
+
+# marks = [9,12,7,10,8,11]
+# print(sum(marks)/len(marks))
+
+
+# name = [ "Gustav", "Albert", "Katie", "Emily", "Tom", "John", "Mike", "Sarah", "Zoe", "Bill", "George"]
+# name.sort()
+# print(name)
+
+
+
+# cities = ["London", "Paris", "Monaco", "Madrid", "Rome"]
+# cities[1] = "Berlin"
+# print(cities[0], cities[-1])
+
+
+songs = ["Hands Up!", "Bad Boys", "Show Me Love", "Whistle", "Gata Only", "La Isla Bonita", "La La La", "What Is Love", "Empire State Of Mind"]
+len(songs)
+print(songs)
+songs.remove("Whistle")
+print(songs)
+songs.sort()
+print(songs)
+songs.reverse()
+print(songs)
