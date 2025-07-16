f99db37b61212a638f7c9a38014beefa0b7753fa
admin2 <https://github.com/Magda233/iasc-marathon>
2025-07-16 11:14:53 +0300

    15 exercises

---
+++ func_tasks.py
@@ -0,0 +1,104 @@
+# def hello_world():
+#     print("Hello, World!")
+
+# hello_world()
+
+
+# greet = ("Anna")
+
+# print("Hello, Anna!")
+
+
+# square = 63* 12
+
+# print(square)
+
+
+# def add(a, b):
+#     return a + b
+# print(add(5,3))
+
+
+
+# def greet(name):
+# 	print(f"How are you, {name}?")
+
+# greet(name="Гість")
+
+
+# factorial = 129
+# print(factorial)
+
+# import math
+
+# print(math.factorial(57))
+# print(math.factorial(23))
+# print(math.factorial(12))
+
+
+# def is_even(number):
+#     if number % 2 == 0:
+#        return True
+#     else:
+#       return False
+# print(is_even(126))
+# print(is_even(13))
+
+# def count_vowels(s):
+#     vowels = "aeiouAEIOU"
+#     count = 0
+#     for char in s:
+#         if char in vowels:
+#             count += 1
+#     return count
+# print(count_vowels("I love to dream about my future"))
+
+
+# def avarage(*numbers):
+#     print(sum(numbers)/len(numbers))
+# avarage(1, 2, 3, 4, 5)
+
+
+# def print_user_info(**info):
+#     print("User name: " + info["name"] + " age: " + str(info["age"]))
+# print_user_info(name="James", age=48)
+
+# def outer():
+#     def inner():
+#         print("Я - вкладена функція!")
+#     inner()
+# outer()
+
+
+# # friends = ["Anna", "Tom", "Bill", "Maks", "Emily", "Samantha"]
+# # find_name = "Oliver"
+# # if find_name in friends:
+# #     print(True)
+# # else:
+# #     print(False)
+
+
+
+# # max_of_three = lambda a, b, c: max(a, b, c)
+# # print(max_of_three(6,9,11))
+
+
+
+# # heart = ("The heart is a muscular organ found in humans and other animals. This organ pumps blood through the blood vessels. The heart and blood vessels together make the circulatory system.")
+# # reversed_heart = heart [::-1]
+# # print(reversed_heart)
+
+
+
+# # text = "I love to dream about my future"
+# # def count_vowels(text):
+# #     vowels = "aeiouAEIOU"
+# #     count = 0
+# #     for char in text:
+# #         if char in vowels:
+# #             count += 1
+# #     return count
+# # print(count_vowels(text))
+
+
+
